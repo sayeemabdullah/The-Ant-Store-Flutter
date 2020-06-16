28ff03138d206424d48c1bc61e74b6edb95136bf
Sayeem Abdullah <sayeem.mdabdullah@gmail.com>
2020-06-17 04:19:29 +0600

    Updated List

--- lib/ProductPage.dart
+++ lib/ProductPage.dart
@@ -15,6 +15,10 @@ class _ProductPageState extends State<ProductPage> {
   List<String>productsAbout = [
     'Carpenter ants are large ants indigenous to many forested parts of the world.' , 'Fire ants are several species of ants in the genus Solenopsis.' , 'Paraponera clavata is a species of ant, commonly known as the bullet ant, named for its extremely potent sting.'
   ];
+
+  List<String> productsPicture = [
+    'blackant.jpg' , 'fireant.jpg' , 'bulletant.jpg'
+  ];
   
   @override
   Widget build(BuildContext context) {
@@ -32,11 +36,24 @@ class _ProductPageState extends State<ProductPage> {
     body: ListView.builder(
       itemCount: productsName.length, 
       itemBuilder: (context, index) {
-        return Card(
-          child: ListTile(
-            onTap: (){},
-            title: Text(productsName[index]),
-            subtitle: Text(productsAbout[index]),
+        return Container(
+          height: 120.0,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 2.5, horizontal: 10.0),
+            child: Card(
+               elevation:5,
+                  margin: EdgeInsets.fromLTRB(0.0, 0.0, 0.0, 16.0),
+                  shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(0.0),
+                  ),
+              child: ListTile(
+                onTap: (){},
+                leading: Image.asset('assets/${productsPicture[index]}',
+                height: 200, width: 80, fit: BoxFit.fitWidth),
+                title: Text(productsName[index]),
+                subtitle: Text(productsAbout[index]),
+              ),
+            ),
           ),
         );
       },
