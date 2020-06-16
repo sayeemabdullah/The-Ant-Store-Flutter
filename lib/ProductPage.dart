b3a29667ca7d64a8e80169db7b4ca2fa71268400
Sayeem Abdullah <sayeem.mdabdullah@gmail.com>
2020-06-16 23:22:03 +0600

    Updated Shopping Cart Icon

--- lib/ProductPage.dart
+++ lib/ProductPage.dart
@@ -10,7 +10,15 @@ class _ProductPageState extends State<ProductPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(
+          title: const Text('Ant Online Store'),
+          actions: <Widget>[
+            IconButton(icon: Icon(
+              Icons.shopping_cart,
+              color: Colors.white,
+            ), onPressed: null)
+          ],
+    ),
     );
   }
 }
