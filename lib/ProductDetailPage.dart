e4b5bb46b3cccc2c0047452253cb35eb490a7dd6
Sayeem Abdullah <sayeem.mdabdullah@gmail.com>
2020-06-17 10:21:37 +0600

    Updated Redesigned Detail Page

--- lib/ProductDetailPage.dart
+++ lib/ProductDetailPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_basic_task/product.dart';
+
 //import 'package:flutter_basic_task/product.dart';
 
 class ProductDetailPage  extends StatelessWidget {
@@ -10,40 +11,17 @@ class ProductDetailPage  extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: CustomScrollView(
-          slivers: <Widget>[
-            SliverAppBar(
-              leading: BackButton(
-                color: Colors.red,
-                ),
-              title: Text('Product Details',
-              style: TextStyle(color: Colors.red),),
-              backgroundColor: Colors.white,
-              expandedHeight: 350.0,
-              actions: <Widget>[
-                IconButton(icon: Icon(
+     appBar: AppBar(
+          title: const Text('Product Details'),
+          backgroundColor: Colors.red,
+          actions: <Widget>[
+            IconButton(icon: Icon(
               Icons.shopping_cart,
-              color: Colors.red,
+              color: Colors.white,
             ), onPressed: null)
-              ],
-              flexibleSpace: FlexibleSpaceBar(
-                centerTitle: true,
-                title: Text(products.name,
-                style: TextStyle(color: Colors.red),),
-                background: Stack(
-                  fit: StackFit.expand,
-                  children: [
-                    Image.asset('assets/${products.picture}',
-                    fit: BoxFit.cover,
-                    ),
-                  ],
-                ),
-              ),
-            ),
           ],
-        )
-      ),
+    ),
+    body: Image.asset('assets/${products.picture}'),
     );
   }
 }
